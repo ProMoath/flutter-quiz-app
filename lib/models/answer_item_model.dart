a797c17789b68abf23fd968ba82fb49faec128f6
Moath Abdo <linktowindows00@gmail.com>
2026-07-29 21:30:40 +0300

    refactor: extract answers lists to a separate file

--- lib/models/answer_item_model.dart
+++ lib/models/answer_item_model.dart
@@ -7,7 +7,7 @@ class AnswerItemModel {
   AnswerItemModel({required this.title, required this.onPressed});
 }
 
-List<AnswerItemModel> answers1 = [
+List<AnswerItemModel> answer1 = [
   AnswerItemModel(
     title: "React.js",
     onPressed: () => debugPrint("React Choice"),
@@ -20,3 +20,37 @@ List<AnswerItemModel> answers1 = [
     onPressed: () => debugPrint("Laravel Choice"),
   ),
 ];
+List<AnswerItemModel> answer2 = [
+  AnswerItemModel(title: "Oracle", onPressed: () => debugPrint("React Choice")),
+  AnswerItemModel(
+    title: "Sql Server",
+    onPressed: () => debugPrint("Next Choice"),
+  ),
+  AnswerItemModel(
+    title: "Postgresql",
+    onPressed: () => debugPrint("Nuxt Choice"),
+  ),
+  AnswerItemModel(title: "MySql", onPressed: () => debugPrint("Node Choice")),
+];
+List<AnswerItemModel> answer3 = [
+  AnswerItemModel(
+    title: "Microsoft",
+    onPressed: () => debugPrint("React Choice"),
+  ),
+  AnswerItemModel(title: "Google", onPressed: () => debugPrint("Next Choice")),
+  AnswerItemModel(
+    title: "Jetbrains",
+    onPressed: () => debugPrint("Nuxt Choice"),
+  ),
+  AnswerItemModel(title: "Node.js", onPressed: () => debugPrint("Node Choice")),
+];
+List<AnswerItemModel> answer4 = [
+  AnswerItemModel(title: "C++", onPressed: () => debugPrint("React Choice")),
+  AnswerItemModel(title: "C#", onPressed: () => debugPrint("Next Choice")),
+  AnswerItemModel(title: "Python", onPressed: () => debugPrint("Nuxt Choice")),
+  AnswerItemModel(
+    title: "TypeScript",
+    onPressed: () => debugPrint("Node Choice"),
+  ),
+];
+List<List<AnswerItemModel>> allAnswers = [answer1, answer2, answer3, answer4];
